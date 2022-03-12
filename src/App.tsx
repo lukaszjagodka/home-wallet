7eb1d5ab7174b0065fc525bfd234cc145218ad08
encorey <kosz.kosz@protonmail.com>
2022-03-12 18:01:09 +0100

    Add LoginForm

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import ChoiceLoginOrCreateAccount from './components/ChoiceLoginOrCreateAccount/
 function App() {
   return (
     <div className="App">
-      <h1>Home-wallet</h1>
       <ChoiceLoginOrCreateAccount />
       test
     </div>
---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './LoginForm.css';
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+
+type TProps = {
+  openList: any,
+}
+
+const LoginForm = function LoginForm({ openList }: TProps) {
+  const [isLoginForm, setIsOpenMenu] = useState(true);
+
+  const handleClose = () => {
+    setIsOpenMenu(false);
+    openList(false);
+  };
+
+  const handleLogin = () => {
+    console.log('send to server');
+  };
+
+  return (
+    <div className="loginContainer">
+      {
+        isLoginForm ? (
+          <div>
+            <DialogTitle className="dialogTitle">Login</DialogTitle>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="accountLogin"
+                label="login"
+                variant="standard"
+                fullWidth
+              />
+              <TextField
+                margin="dense"
+                id="accountPassword"
+                label="password"
+                variant="standard"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions className="dialogActions" style={{ padding: '25px' }}>
+              <Button variant="outlined" onClick={handleClose}>Cancel</Button>
+              <Button variant="outlined" color="success" onClick={handleLogin}>Login</Button>
+            </DialogActions>
+          </div>
+        ) : ''
+      }
+    </div>
+  );
+};
+
+export default LoginForm;
