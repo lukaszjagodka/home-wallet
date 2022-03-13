909388a3ee5980ff347dc46ecc55aff16bf92543
encorey <kosz.kosz@protonmail.com>
2022-03-13 01:00:27 +0100

    Add Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = function Navbar() {
+  return (
+    <div className="container">
+      <div className="navbarName">Home Wallet</div>
+    </div>
+  );
+};
+
+export default Navbar;
