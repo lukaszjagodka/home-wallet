e6b4d0cf06531e4d99131c7a66e1007a36ecacd6
encorey <kosz.kosz@protonmail.com>
2022-03-16 18:59:38 +0100

    Add transactionList

---
+++ src/components/Transaction/Transaction.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Transaction.css';
+
+const Transaction = function Transaction() {
+  return (
+    <div className="singleTransaction">Transaction</div>
+  );
+};
+
+export default Transaction;
---
+++ src/components/TransactionsList/TransactionsList.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './TransactionsList.css';
+
+import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
+import Collapse from '@mui/material/Collapse';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Transaction from '../Transaction/Transaction';
+
+const TransactionsList = function TransactionsList() {
+  const [checked, setChecked] = useState(false);
+  const transArr: any[] = [];
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
+  setTimeout(() => {
+    setChecked(true);
+  }, 1000);
+
+  return (
+    <div className="transactionsList">
+      <Collapse in={checked}>
+        <div className="transactionsComponent">
+          <Box sx={{ height: 35, backgroundColor: 'rgba(244, 245, 245, 0.835)' }}>
+            <FormControlLabel
+              control={<Switch checked={checked} onChange={handleChange} />}
+              label="Show"
+            />
+          </Box>
+          {
+            transArr.map((transactionObj) => (
+              <Transaction />
+            ))
+          }
+          <Transaction />
+          <Transaction />
+          <Transaction />
+        </div>
+      </Collapse>
+    </div>
+  );
+};
+
+export default TransactionsList;
