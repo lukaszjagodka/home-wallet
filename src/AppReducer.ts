72df11f7b1eddaadef7fcbdf356aca4bcf9e0024
encorey <kosz.kosz@protonmail.com>
2022-03-19 20:01:06 +0100

    transactionsReducer,

--- src/AppReducer.ts
+++ src/AppReducer.ts
@@ -1,3 +1,8 @@
 import { combineReducers } from 'redux';
+import transactionsReducer from './components/transactions/transactionsReducer';
 
-export const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  transactionsReducer,
+});
+
+export default rootReducer;
