6e30418f978f11b691f84cf5a489d1acaeb9c3c6
encorey <kosz.kosz@protonmail.com>
2022-03-22 12:51:20 +0100

    Delete unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import React, { useState } from 'react';
-import ChoiceLoginOrCreateAccount from './components/ChoiceLoginOrCreateAccount/ChoiceLoginOrCreateAccount';
+import React from 'react';
 import Dashboard from './components/Dashboard/Dashboard';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  const [isLogged, setIsLogged] = useState(false);
-  const [isX, setIsX] = useState(false);
-  if (!isX) {
-    const retrievedSelectedAccObject: string | null = localStorage.getItem('selectedAccount');
-    if (retrievedSelectedAccObject) {
-      setIsLogged(true);
-      setIsX(true);
-    }
-  }
   return (
     <div className="App">
-      {
-        isLogged
-          ? (
-            <>
-              <Navbar />
-              <Dashboard />
-            </>
-          ) : <ChoiceLoginOrCreateAccount />
-      }
+      <Navbar />
+      <Dashboard />
     </div>
   );
 }
