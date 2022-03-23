a7f7042ccc5129ad0745ab8dead1051e8b160e7a
encorey <kosz.kosz@protonmail.com>
2022-03-23 23:33:03 +0100

    Change css in TransactionList

--- src/components/transactions/TransactionsList/TransactionsList.tsx
+++ src/components/transactions/TransactionsList/TransactionsList.tsx
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import './TransactionsList.css';
 
-import Box from '@mui/material/Box';
-import Switch from '@mui/material/Switch';
 import Collapse from '@mui/material/Collapse';
-import FormControlLabel from '@mui/material/FormControlLabel';
 import TransactionItem from '../Transaction/TransactionItem';
 import { TNewTransaction, TTransactionsOnList } from '../../../types/types';
 
@@ -27,12 +24,7 @@ const TransactionsList = function TransactionsList() {
     <div className="transactions-list">
       <Collapse in={checked}>
         <div className="transactions-component">
-          <Box className="box-transactions-component">
-            <FormControlLabel
-              control={<Switch checked={checked} onChange={handleChange} />}
-              label="Show"
-            />
-          </Box>
+          <div className="box-transactions-component" />
           {transactions.map((transactionObj: TNewTransaction) => (
             <TransactionItem key={transactionObj.amount} params={transactionObj} />
           ))}
