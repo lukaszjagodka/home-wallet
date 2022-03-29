05cdcd6f74800876985ee6705e570f422d365e57
encorey <kosz.kosz@protonmail.com>
2022-03-29 17:05:31 +0200

    Add editTransaction action and reducer

--- src/components/transactions/transactionsActions.ts
+++ src/components/transactions/transactionsActions.ts
@@ -1,6 +1,11 @@
-import { TNewTransaction } from '../../types/types';
+import { TEditTransaction, TNewTransaction } from '../../types/types';
 
 export const addTransaction = (payload: TNewTransaction) => ({
   type: 'ADD_TRANSACTION',
+  payload,
+});
+
+export const editTransaction = (payload: TEditTransaction) => ({
+  type: 'EDIT_TRANSACTION',
   payload,
 });
--- src/components/transactions/transactionsReducer.ts
+++ src/components/transactions/transactionsReducer.ts
@@ -1,8 +1,8 @@
+/* eslint-disable no-case-declarations */
 import { TTransactions, TTransactionAction } from '../../types/types';
 
 const initialState = {
-  transactions: [
-  ],
+  transactions: [],
 };
 
 const transactionsReducer = (state: TTransactions = initialState, action: TTransactionAction) => {
@@ -11,6 +11,16 @@ const transactionsReducer = (state: TTransactions = initialState, action: TTrans
       return {
         ...state,
         transactions: [action.payload, ...state.transactions],
+      };
+    case 'EDIT_TRANSACTION':
+      let updatedTransaction: any = {};
+      const editObj = action.payload;
+      if (editObj) {
+        updatedTransaction = state.transactions.map((transaction) => (transaction.id === editObj.id ? { ...transaction, description: editObj.description, amount: editObj.amount } : transaction));
+      }
+      return {
+        ...state,
+        transactions: updatedTransaction,
       };
     default:
       return state;
