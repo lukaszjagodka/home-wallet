b827ad3546e58d2dc8975cc534be573c9adc5293
encorey <kosz.kosz@protonmail.com>
2022-04-03 19:30:53 +0200

    Use useEffect

--- src/helpers/getDaysInMonth.ts
+++ src/helpers/getDaysInMonth.ts
@@ -1,13 +1,8 @@
-/* eslint-disable no-shadow */
-import { useDispatch } from 'react-redux';
-import { changeDaysInChart } from '../components/accountActions';
-
 export const getDaysInMonth = (date: Date = new Date()) => {
-  const dispatch = useDispatch();
   const now = new Date();
   const month = date.getMonth();
   const year = now.getFullYear();
   const daysInMonth = new Date(year, month + 1, 0).getDate();
   const array = [daysInMonth, month];
-  dispatch(changeDaysInChart(array));
+  return array;
 };
