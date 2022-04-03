998e759a674c13b64689cd411ac33622d331ee37
encorey <kosz.kosz@protonmail.com>
2022-04-03 22:56:31 +0200

    Refactor transactionsReducer

--- src/components/transactions/transactionsReducer.ts
+++ src/components/transactions/transactionsReducer.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-case-declarations */
-import { TTransactions, TTransactionAction } from '../../types/types';
+import { TTransactions, TTransactionAction, TNewTransaction } from '../../types/types';
 
 const initialState = {
   transactions: [],
@@ -13,11 +13,7 @@ const transactionsReducer = (state: TTransactions = initialState, action: TTrans
         transactions: [action.payload, ...state.transactions],
       };
     case 'EDIT_TRANSACTION':
-      let updatedTransaction: any = {};
-      const editObj = action.payload;
-      if (editObj) {
-        updatedTransaction = state.transactions.map((transaction) => (transaction.id === editObj.id ? { ...transaction, description: editObj.description, amount: editObj.amount } : transaction));
-      }
+      const updatedTransaction = state.transactions.map((transaction) => (transaction.id === action.payload.id ? { ...transaction, description: action.payload.description, amount: action.payload.amount } : transaction));
       return {
         ...state,
         transactions: updatedTransaction,
@@ -25,9 +21,7 @@ const transactionsReducer = (state: TTransactions = initialState, action: TTrans
     case 'DELETE_TRANSACTION':
       const index = state.transactions.findIndex((item) => item.id === action.payload);
       if (index !== -1) { state.transactions.splice(index, 1); }
-      return {
-        ...state,
-      };
+      return state;
     default:
       return state;
   }
